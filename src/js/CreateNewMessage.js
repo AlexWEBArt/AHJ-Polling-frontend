df5886951d9718da5fc340b45d62921c2913cd7f
Артемчук Алексей <elusiv@mail.ru>
2023-03-27 00:20:20 +0600

    first commit 26.03.23

---
+++ src/js/CreateNewMessage.js
@@ -0,0 +1,45 @@
+import getCreationDate from './getCreationDate';
+
+export default class CreateNewMessage {
+  constructor(conteiner) {
+    this.container = conteiner;
+  }
+
+  renderMessage(item) {
+    const listMessage = document.createElement('UL');
+    const liEmail = document.createElement('LI');
+    const paragraphEmail = document.createElement('P');
+    const liSubject = document.createElement('LI');
+    const paragraphSubject = document.createElement('P');
+    const liCreated = document.createElement('LI');
+    const paragraphCreated = document.createElement('P');
+
+    listMessage.classList.add('list-message');
+    listMessage.setAttribute('id', item.id);
+    liEmail.classList.add('email');
+    paragraphEmail.classList.add('email-text');
+
+    liSubject.classList.add('subject');
+    paragraphSubject.classList.add('subject-text');
+
+    liCreated.classList.add('created');
+    paragraphCreated.classList.add('date_of_creation');
+
+    paragraphEmail.textContent = item.from;
+
+    if (item.subject.length > 15) {
+      paragraphSubject.textContent = `${item.subject.slice(0, 15)}...`;
+    } else {
+      paragraphSubject.textContent = item.subject;
+    }
+    paragraphCreated.textContent = getCreationDate(item.received);
+
+    this.container.prepend(listMessage);
+    listMessage.append(liEmail);
+    listMessage.append(liSubject);
+    listMessage.append(liCreated);
+    liEmail.append(paragraphEmail);
+    liSubject.append(paragraphSubject);
+    liCreated.append(paragraphCreated);
+  }
+}
---
+++ src/js/app.js
@@ -0,0 +1,66 @@
+import {
+  catchError, of,
+} from 'rxjs';
+// import { Observable, map, interval } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import CreateNewMessage from './CreateNewMessage';
+
+import background from '../img/cell.jpg';
+
+document.querySelector('body').style.backgroundImage = `url(${background})`;
+
+const mailBox = document.querySelector('.container-mail');
+
+const message = new CreateNewMessage(mailBox);
+
+// const stream$ = new Observable( (observer) => {
+//   setInterval(() => {
+//     const request = fetch(`http://localhost:7070/messages/unread`, {
+//       method: 'GET',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//     });
+
+//     observer.next(request);
+//   }, 1000)
+// })
+
+const obs$ = ajax.getJSON('http://localhost:7070/messages/unread')
+  .pipe(
+    // interval(1000),
+    // map((userResponse) => {
+    //   console.log(userResponse);
+    //   return userResponse;
+    // }),
+    catchError((error) => {
+      console.log('error: ', error);
+      return of(error);
+    }),
+  );
+
+obs$.subscribe({
+  next: (value) => {
+    const idList = Array.from(document.querySelectorAll('.list-message')).map((item) => item.getAttribute('id'));
+    value.messages.forEach((item) => {
+      if (!idList.includes(item.id)) {
+        message.renderMessage(item);
+      }
+    });
+  },
+  error: (err) => console.log(err),
+});
+
+// stream$.subscribe(async (value) => {
+//   const idList = Array.from(document.querySelectorAll('.list-message'))
+//     .map(item => item.getAttribute('id'));
+
+//   const result = await value
+//   const json = await result.json()
+
+// json.messages.forEach(item => {
+//   if (!idList.includes(item.id)) {
+//     message.renderMessage(item)
+//   }
+//   })
+// });
