be0d30f1831ac2b629543c3ffbb676006778ca35
Артемчук Алексей <elusiv@mail.ru>
2023-03-27 16:15:48 +0600

    added interval polling 26.03.23

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,6 @@
 import {
-  catchError, of,
+  catchError, of, interval, mergeMap,
 } from 'rxjs';
-// import { Observable, map, interval } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
 import CreateNewMessage from './CreateNewMessage';
 
@@ -13,54 +12,29 @@ const mailBox = document.querySelector('.container-mail');
 
 const message = new CreateNewMessage(mailBox);
 
-// const stream$ = new Observable( (observer) => {
-//   setInterval(() => {
-//     const request = fetch(`http://localhost:7070/messages/unread`, {
-//       method: 'GET',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//     });
+const ajaxInterval$ = interval(1000);
 
-//     observer.next(request);
-//   }, 1000)
-// })
-
-const obs$ = ajax.getJSON('https://polling-backend-panw.onrender.com/messages/unread')
+ajaxInterval$
   .pipe(
-    // interval(1000),
-    // map((userResponse) => {
-    //   console.log(userResponse);
-    //   return userResponse;
-    // }),
-    catchError((error) => {
-      console.log('error: ', error);
-      return of(error);
-    }),
-  );
-
-obs$.subscribe({
-  next: (value) => {
-    const idList = Array.from(document.querySelectorAll('.list-message')).map((item) => item.getAttribute('id'));
-    value.messages.forEach((item) => {
-      if (!idList.includes(item.id)) {
-        message.renderMessage(item);
+    mergeMap(() => ajax.getJSON('http://localhost:7070/messages/unread')
+      .pipe(
+        catchError((error) => {
+          console.log('error: ', error);
+          return of(null);
+        }),
+      )),
+  )
+  .subscribe({
+    next: (value) => {
+      console.log(value);
+      const idList = Array.from(document.querySelectorAll('.list-message')).map((item) => item.getAttribute('id'));
+      if (value) {
+        value.messages.forEach((item) => {
+          if (!idList.includes(item.id)) {
+            message.renderMessage(item);
+          }
+        });
       }
-    });
-  },
-  error: (err) => console.log(err),
-});
-
-// stream$.subscribe(async (value) => {
-//   const idList = Array.from(document.querySelectorAll('.list-message'))
-//     .map(item => item.getAttribute('id'));
-
-//   const result = await value
-//   const json = await result.json()
-
-// json.messages.forEach(item => {
-//   if (!idList.includes(item.id)) {
-//     message.renderMessage(item)
-//   }
-//   })
-// });
+    },
+    error: (err) => console.log(err),
+  });
